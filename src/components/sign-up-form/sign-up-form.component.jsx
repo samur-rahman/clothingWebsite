91accd688beba66d78b6ebcf94debb9bad14fe4e
samur-rahman <ahotasam@buffalo.edu>
2023-07-21 02:54:14 -0400

    Practiced the signIn with googleredirect and created the component for the signup form.

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
+
+  console.log(formFields);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    // spreads the formFields and based on event.target it updates that value of the elements
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div>
+      <h1>Sign Up with your email and password</h1>
+      <form onSubmit={() => {}}>
+        <label>Display Name</label>
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
+
+        <label>Email</label>
+        <input
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,15 +1,27 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import { useEffect } from "react";
+import { getRedirectResult } from "firebase/auth";
+
+import {
+  auth,
+  signInWithGooglePopup,
+  signInWithGoogleRedirect,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
-    const logGoogleUser = async() => {
-        const {user} = await signInWithGooglePopup();
-        console.log(user);
-        const userDocRef = await createUserDocumentFromAuth(user)
-    }
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    console.log(user);
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
   return (
     <div>
       <h1>This is Sign In page.</h1>
       <button onClick={logGoogleUser}> Sign In with Google popup.</button>
+      <SignUpForm />
     </div>
   );
 };
